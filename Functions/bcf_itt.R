c9a02dba954349f66794a76673c3116e60482a3b
Falco J. Bargagli-Stoffi <36726286+fbargaglistoffi@users.noreply.github.com>
2022-03-29 10:52:23 -0400

    Separate BCF-IV & BCF-ITT

--- Functions/bcf_itt.R
+++ Functions/bcf_itt.R
@@ -1,21 +1,14 @@
-##### bcf_iv: function for discovery and estimation of heterogeneity in the Complier Average Causal Effect in Instrumental Variable settings
+##### bcf_itt: function for discovery and estimation of heterogeneity in the Intention-To-Treat in Instrumental Variable settings
 ### INPUTs:y: outcome vector (n x 1)
-###        w: vector of treatment received  (n x 1)
 ###        z: vector of treatment assigned (aka instrumental variable)  (n x 1)
 ###        x: covariates matrix (n x p) 
-###        binary: Boolean to identify whether the outcome is binary (TRUE) or continuous/discrete (FALSE) 
 ###        n_burn: burn-in MCMC iterations for Bayesian Causal Forest
 ###        n_sim: iterations to save post burn-in for Bayesian Causal Forest
 ###        inference_ratio: % of observations to be assigned to the inference subsample (default is 0.5)
 ###        max_depth: maximal depth for the generated CART (default is 2)
-###        cp: complexity parameter for the generated CART (default is 0.01)
-###        minsplit: minimum observations needed to perform a binary split in the tree (default is 10)
-###        adj_method: p-value adjustment method (default is "holm"), other options are "bonferroni", "hockberg", "hommel", "BH", "BY", "fdr", "none"
-###        seed: random seed for reproducible results (default is 42)
-### OUTPUTS: (1) a tree structure discovering the heterogeneity in the causal effects, (2) TSLS estimates of the Complier Average Causal Effects (CCACE) within its nodes, (3) p-values for each CCACE, (4) adjusted p-value for each CCACE, (5) Intention-to-Treat within all the nodes, (6) proportion of Compliers within all the nodes, (7) p-value for weak-iv test, (8) proportion of observations in the node 
+### OUTPUTS: (1) a tree structure discovering the heterogeneity in the causal effects, (2) TSLS estimates of the Complier Average Causal Effects (CCACE) within its nodes, (3) p-values for each CCACE, (4) p-value for weak-iv test
 
-bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, inference_ratio = 0.5, 
-                   max_depth = 2, cp = 0.01, minsplit = 10, adj_method = "holm", seed = 42) {
+bcf_itt <- function(y, w, z, x, max_depth, n_burn, n_sim, inference_ratio, binary = FALSE) {
   
   # Upload the Packages
   require(bcf)
@@ -30,7 +23,6 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
   ######################################################
   
   # Split data into Discovery and Inference
-  set.seed(seed)
   index <- sample(nrow(x), nrow(x)*inference_ratio, replace=FALSE)
   
   # Initialize total dataset
@@ -40,6 +32,7 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
   # Discovery and Inference Samples
   discovery <- iv.data[-index,]
   inference <- iv.data[index,]
+  
   
   ######################################################
   ####  Step 1: Compute the Bayesian Causal Forest  ####
@@ -51,10 +44,6 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
                  data = discovery)
   pihat <- predict(p.score, as.data.frame(x[-index,]))
   
-  # Perform the Bayesian Causal Forest for the Proportion of Compliers (pic)
-  pic_bcf <- quiet(bartc(w[-index], z[-index], x[-index,], n.samples = n_sim, n.burn = n_burn, n.chains = 2L))
-  tau_pic <- extract(pic_bcf, type = "ite")
-  pic <- apply(tau_pic, 2, mean)
   
   ######################################################
   ####     Continuous and Discrete Outcomes         ####
@@ -63,12 +52,11 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
   if (binary == FALSE){
     
     # Perform the Bayesian Causal Forest for the ITT
-    itt_bcf <- quiet(bcf(y[-index], z[-index], x[-index,], x[-index,], pihat, nburn=n_burn, nsim=n_sim))
-    tau_itt <- itt_bcf$tau
-    itt <- colMeans(tau_itt)
+    bcf_fit <- quiet(bcf(y[-index], z[-index], x[-index,], x[-index,], pihat, nburn=n_burn, nsim=n_sim))
     
-    # Get posterior of treatment effects
-    tauhat <- itt/pic
+    # Compute Posterior
+    tau_post <- bcf_fit$tau
+    tauhat <- colMeans(tau_post)
     exp <- as.data.frame(cbind(tauhat, x[-index,]))
     
     ######################################################
@@ -77,9 +65,7 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
     
     fit.tree <- rpart(tauhat ~ .,
                       data = exp,
-                      maxdepth = max_depth,
-                      cp=cp,
-                      minsplit=minsplit)
+                      maxdepth = max_depth)
     
     ######################################################
     ####  Step 3: Extract the Causal Rules (Nodes)    ####
@@ -87,14 +73,8 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
     
     rules <- as.numeric(row.names(fit.tree$frame[fit.tree$numresp]))
     
-    # Initialize Outputs
-    bcfivMat <- as.data.frame(matrix(NA, nrow = length(rules), ncol=7))
-    names(bcfivMat) <- c("node", "CCACE", "pvalue", "Weak_IV_test", "Pi_obs", "ITT", "Pi_compliers")
-    
-    # Generate Leaves Indicator
-    lvs <- leaves <- numeric(length(rules)) 
-    lvs[unique(fit.tree$where)] <- 1
-    leaves[rules[lvs==1]] <- 1
+    # Exclude the Root
+    rules <- rules[-1] 
     
     ######################################################
     ####  Step 4: Run an IV Regression on each Node   ####
@@ -107,39 +87,38 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
     iv.effect.root <-  summary$coef[2,1]
     p.value.root <- summary$coef[2,4]
     p.value.weak.iv.root <- summary$diagnostics[1,4]
-    proportion.root <- 1
-    compliers.root <- length(which(inference$z==inference$w))/nrow(inference)
-    itt.root <- iv.effect.root*compliers.root
     
-    # Store Results for Root
-    bcfivMat[1,] <- c( NA , round(iv.effect.root, 4), round(p.value.root, 4), round(p.value.weak.iv.root, 4), round(proportion.root, 4), round(itt.root, 4), round(compliers.root, 4))
+    # Print Root Results
+    cat(paste("The effect on the overall sample is", round(iv.effect.root, 4)),"\n")
+    cat(paste("P-value", p.value.root),"\n")
+    cat(paste("P-value Weak-Instrument Test", p.value.weak.iv.root),"\n")
+    cat(paste("Proportion of observations in the node: ", "1.00"),"\n")
     
     # Initialize New Data
     names(inference) <- paste(names(inference), sep="")
     
     # Run a loop to get the rules (sub-populations)
-    for (i in rules[-1]){
+    for (i in rules){
       # Create a Vector to Store all the Dimensions of a Rule
       sub <- as.data.frame(matrix(NA, nrow = 1,
-                                  ncol = nrow(as.data.frame(path.rpart(fit.tree, node=i, print.it = FALSE)))-1))
+                                  ncol = nrow(as.data.frame(path.rpart(fit.tree,node=i)))-1))
       quiet(capture.output(for (j in 1:ncol(sub)){
         # Store each Rule as a Sub-population
-        sub[,j] <- as.character(print(as.data.frame(path.rpart(fit.tree,node=i,print.it=FALSE))[j+1,1]))
-        sub_pop <- noquote(paste(sub , collapse = " & "))
+        sub[,j] <- as.character(print(as.data.frame(path.rpart(fit.tree,node=i))[j+1,1]))
+        sub_pop <- noquote(paste(sub, collapse = " & "))
       }))
+      
       
       subset <- with(inference, inference[which(eval(parse(text=sub_pop))),])
       
       # Run the IV Regression
       if (length(unique(subset$w))!= 1 | length(unique(subset$z))!= 1){
-        iv.reg <- ivreg(y ~ w | z,  
+        iv.reg <- ivreg(y ~ w | z ,  
                         data = subset)
         summary <- summary(iv.reg, diagnostics = TRUE)
         iv.effect <-  summary$coef[2,1]
         p.value <- summary$coef[2,4]
         p.value.weak.iv <- summary$diagnostics[1,4]
-        compliers <- length(which(subset$z==subset$w))/nrow(subset)
-        itt <- iv.effect*compliers
         
         # Proportion of observations in the node
         proportion.node <- nrow(subset)/nrow(inference)
@@ -148,21 +127,17 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
         ####   Step 5: Output the Values of each CCACE   ####
         ######################################################
         
-        bcfivMat[i,] <- c(sub_pop, round(iv.effect, 4), round(p.value, 4), round(p.value.weak.iv, 4), round(proportion.node, 4), round(itt, 4), round(compliers, 4))
+        cat(paste("The conditional effect on the subpopulation is", round(iv.effect, 4)),"\n")
+        cat(paste("P-value", p.value),"\n")
+        cat(paste("P-value Weak-Instrument Test", p.value.weak.iv),"\n")
+        cat(paste("Proportion of observations in the node: ", proportion.node),"\n")
+        
       }
       
       # Delete data
       rm(subset)
+      
     }
-    
-    # Adjust P.values 
-    bcfiv_correction <- cbind(as.data.frame(bcfivMat), leaves)
-    adj <- round(p.adjust( as.numeric(bcfiv_correction$pvalue[which(bcfiv_correction$leaves==1)]) ,  paste(adj_method)), 5)
-    Adj_pvalue <- rep(NA, length(rules)) 
-    Adj_pvalue[which(bcfiv_correction$leaves==1)] <- adj
-    
-    # Store Results
-    bcfivResults <- cbind(as.data.frame(bcfivMat), Adj_pvalue)
   }
   
   ######################################################
@@ -171,26 +146,21 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
   
   if (binary == TRUE){
     
-    # Perform the binary Bayesian Causal Forest for the ITT
+    # Fit Binary BART (from BARTcause)
     fit_itt <- quiet(bartc(y[-index], z[-index], x[-index,], n.samples = n_sim, n.burn = n_burn, n.chains = 2L))
     
-    # Get posterior of treatment effects
+    # Get Posterior of Treatment Effects
     ites <- extract(fit_itt, type = "ite")
-    itt <- apply(ites, 2, mean)
-    
-    # Get posterior of treatment effects
-    tauhat <- itt/pic
+    tauhat <- apply(ites, 2, mean)
     exp <- as.data.frame(cbind(tauhat, x[-index,]))
     
     ######################################################
     ####  Step 2: Build a CART on the Unit Level CITT ####
     ######################################################
     
-    fit.tree <- rpart(tauhat ~ .,
+    fit.tree <- rpart(tauhat ~.,
                       data = exp,
-                      maxdepth = max_depth,
-                      cp=cp,
-                      minsplit=minsplit)
+                      maxdepth = max_depth)
     
     ######################################################
     ####  Step 3: Extract the Causal Rules (Nodes)    ####
@@ -198,44 +168,39 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
     
     rules <- as.numeric(row.names(fit.tree$frame[fit.tree$numresp]))
     
-    # Initialize Outputs
-    bcfivMat <- as.data.frame(matrix(NA, nrow = length(rules), ncol=7))
-    names(bcfivMat) <- c("node", "CCACE", "pvalue", "Weak_IV_test", "Pi_obs", "ITT", "Pi_compliers")
+    # Exclude the Root
+    rules <- rules[-1] 
     
-    # Generate Leaves Indicator
-    lvs <- leaves <- numeric(length(rules)) 
-    lvs[unique(fit.tree$where)] <- 1
-    leaves[rules[lvs==1]] <- 1
     
     ######################################################
     ####  Step 4: Run an IV Regression on each Node   ####
     ######################################################
     
     # Run an IV Regression on the Root
-    iv.root <- ivreg(y ~ w | z,  
+    iv.root <- ivreg(y ~ w| z ,  
                      data = inference)
     summary <- summary(iv.root, diagnostics = TRUE)
     iv.effect.root <-  summary$coef[2,1]
     p.value.root <- summary$coef[2,4]
     p.value.weak.iv.root <- summary$diagnostics[1,4]
-    proportion.root <- 1
-    compliers.root <- length(which(inference$z==inference$w))/nrow(inference)
-    itt.root <- iv.effect.root*compliers.root
     
-    # Store Results for Root
-    bcfivMat[1,] <- c( NA , round(iv.effect.root, 4), round(p.value.root, 4), round(p.value.weak.iv.root, 4), round(proportion.root, 4), round(itt.root, 4), round(compliers.root, 4))
+    # Print Root Results
+    cat(paste("The effect on the overall sample is", round(iv.effect.root, 4)),"\n")
+    cat(paste("P-value", p.value.root),"\n")
+    cat(paste("P-value Weak-Instrument Test", p.value.weak.iv.root),"\n")
+    cat(paste("Proportion of observations in the node: ", "1.00"),"\n")
     
     # Initialize New Data
     names(inference) <- paste(names(inference), sep="")
     
     # Run a loop to get the rules (sub-populations)
-    for (i in rules[-1]){
+    for (i in rules){
       # Create a Vector to Store all the Dimensions of a Rule
       sub <- as.data.frame(matrix(NA, nrow = 1,
-                                  ncol = nrow(as.data.frame(path.rpart(fit.tree, node=i, print.it = FALSE)))-1))
+                                  ncol = nrow(as.data.frame(path.rpart(fit.tree,node=i)))-1))
       quiet(capture.output(for (j in 1:ncol(sub)){
         # Store each Rule as a Sub-population
-        sub[,j] <- as.character(print(as.data.frame(path.rpart(fit.tree,node=i,print.it=FALSE))[j+1,1]))
+        sub[,j] <- as.character(print(as.data.frame(path.rpart(fit.tree,node=i))[j+1,1]))
         sub_pop <- noquote(paste(sub , collapse = " & "))
       }))
       
@@ -243,14 +208,13 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
       
       # Run the IV Regression
       if (length(unique(subset$w))!= 1 | length(unique(subset$z))!= 1){
-        iv.reg <- ivreg(y ~ w | z,  
+        
+        iv.reg <- ivreg(y ~ w | z ,  
                         data = subset)
         summary <- summary(iv.reg, diagnostics = TRUE)
         iv.effect <-  summary$coef[2,1]
         p.value <- summary$coef[2,4]
         p.value.weak.iv <- summary$diagnostics[1,4]
-        compliers <- length(which(subset$z==subset$w))/nrow(subset)
-        itt <- iv.effect*compliers
         
         # Proportion of observations in the node
         proportion.node <- nrow(subset)/nrow(inference)
@@ -259,27 +223,19 @@ bcf_iv <- function(y, w, z, x, binary = FALSE, n_burn = 500, n_sim = 500, infere
         ####   Step 5: Output the Values of each CCACE   ####
         ######################################################
         
-        bcfivMat[i,] <- c(sub_pop, round(iv.effect, 4), round(p.value, 4), round(p.value.weak.iv, 4), round(proportion.node, 4), round(itt, 4), round(compliers, 4))
+        cat(paste("The conditional effect on the subpopulation is", round(iv.effect, 4)),"\n")
+        cat(paste("P-value", p.value),"\n")
+        cat(paste("P-value Weak-Instrument Test", p.value.weak.iv),"\n")
+        cat(paste("Proportion of observations in the node: ", proportion.node),"\n")
+        
       }
       
       # Delete data
       rm(subset)
     }
-    
-    # Adjust P.values 
-    bcfiv_correction <- cbind(as.data.frame(bcfivMat), leaves)
-    adj <- round(p.adjust( as.numeric(bcfiv_correction$pvalue[which(bcfiv_correction$leaves==1)]) ,  paste(adj_method)), 5)
-    Adj_pvalue <- rep(NA, length(rules)) 
-    Adj_pvalue[which(bcfiv_correction$leaves==1)] <- adj
-    
-    # Store Results
-    bcfivResults <- cbind(as.data.frame(bcfivMat), Adj_pvalue)
   }
   
-  # Return Results
-  return(bcfivResults)
 }
-
 
 # Don't print in-function messages (source: https://r.789695.n4.nabble.com/Suppressing-output-e-g-from-cat-td859876.html)
 quiet <- function(x) { 
