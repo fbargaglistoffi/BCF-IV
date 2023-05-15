53a2b39d59f1619659fbb48643aea854f5c3259c
riccardocadei <riccardo.cadei98@gmail.com>
2023-05-15 12:17:54 -0400

    Add functions example

---
+++ simulations/example_BCF-IV.R
@@ -0,0 +1,19 @@
+# Generate synthetic dataset
+dataset <- generate_dataset(n = 1000, 
+                            p = 10, 
+                            rho = 0, 
+                            null = 0, 
+                            effect_size = 2, 
+                            compliance = 0.75)
+y <- dataset[["y"]]
+w <- dataset[["w"]]
+z <- dataset[["z"]]
+X <- dataset[["X"]]
+
+bcf_iv(y, w, z, X, 
+       n_burn = 2000, 
+       n_sim = 2000, 
+       inference_ratio = 0.5, 
+       binary = FALSE, 
+       max_depth = 2, 
+       adj_method = "holm")
